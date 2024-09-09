0beba30de785cd59022b48592187d69a6a31e130
TienDungVo <dung81votien1872004@gmail.com>
2024-09-09 13:53:57 +0700

    demo

---
+++ src/main/java/com/example/goblinslayer/GoblinSlayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.goblinslayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoblinSlayerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoblinSlayerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goblinslayer/GoblinSlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goblinslayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoblinSlayerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
